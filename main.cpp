00a86313b87069f2de9c2139687dc900c1977b0c
abramovgeorge <abramov.g.evg@gmail.com>
2022-05-11 14:30:10 +0300

    Direct functions implemented

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <QApplication>
-#include <QMainWindow>
 #include <fstream>
-#include "include/json.hpp"
+#include <cmath>
 #include "src/function.cpp"
 #include "src/graph.cpp"
 #include "src/plane.cpp"
 
 using namespace std;
-using json = nlohmann::json;
+
+double in_func(double x) {
+    return (pow((x + 3), sin(x + 3))-3)/100;
+}
+
+const int in_color = 1;
+const bool in_grid = true;
+const double in_a = -2;
+const double in_b = 10;
+const int in_x1 = 0;
+const int in_y1 = 0;
+const int in_x2 = 700;
+const int in_y2 = 700;
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    fstream in("../config.json");
-    json config;
-    in >> config;
-    settings.setSettings(config["x1"], config["y1"], config["x2"], config["y2"], config["grid"]);
-    Function func = Function(config["function"]);
-    Graph graph = Graph(&func, config["a"], config["b"]);
-    Plane plane = Plane(&graph, COLOR(config["color"]));
+    settings.setSettings(in_x1, in_y1, in_x2, in_y2, in_grid);
+    Function func = Function(in_func);
+    Graph graph = Graph(&func, in_a, in_b);
+    Plane plane = Plane(&graph, COLOR(in_color));
     QLabel l;
     l.setPicture(plane.getPicture());
     l.setWindowTitle("Universal Graph Builder");
--- src/function.cpp
+++ src/function.cpp
@@ -8,113 +8,16 @@ using namespace std;
 
 class Function {
 public:
-    Function(string s) {
-        func_ = s;
+    Function(double (*f)(double)) {
+        func_ = f;
     }
     double calculate(double x) {
-        i_ = 0;
-        string pre_func = func_;
-        func_ += '#';
-        double res = add_(x);
-        if (func_[i_] == '#' && i_ == func_.size() - 1) {
-            func_ = pre_func;
-            return res;
-        }
-        else {
-            func_ = pre_func;
-            return 0.0; //incorrect equation
-        }
+        return func_(x);
     }
 
 private:
-    string func_;
+    double (*func_)(double);
     int i_ = 0;
-
-    double add_(double x) {
-        double res = mult_(x);
-        while (func_[i_] == '+') {
-            i_++;
-            res += mult_(x);
-        }
-        return res;
-    }
-
-    double mult_(double x) {
-        double res = power_(x);
-        while (func_[i_] == '*' or func_[i_] == '/') {
-            if (func_[i_] == '*') {
-                i_++;
-                res *= power_(x);
-            }
-            else {
-                i_++;
-                res /= power_(x);
-            }
-        }
-        return res;
-    }
-
-    double power_(double x) {
-        double res = factor_(x);
-        while (func_[i_] == '^') {
-            i_++;
-            res = pow(res, factor_(x));
-        }
-        return res;
-    }
-
-    double factor_(double x) {
-        double res = 0.0;
-        switch (func_[i_]) {
-            case 'x':
-                res = x;
-                i_++;
-                break;
-            case '0'...'9':
-                while (func_[i_] >= '0' && func_[i_] <= '9') {
-                    res = res * 10 + func_[i_] - '0';
-                    i_++;
-                }
-                break;
-            case '-':
-                i_++;
-                res -= factor_(x);
-                break;
-            case 's':
-                i_++;
-                res = sin(factor_(x));
-                break;
-            case 'c':
-                i_++;
-                res = cos(factor_(x));
-                break;
-            case 't':
-                i_++;
-                res = tan(factor_(x));
-                break;
-            case 'l':
-                i_++;
-                res = log(factor_(x));
-                break;
-            case '(':
-                i_++;
-                res = add_(x);
-                if (func_[i_] == ')') {
-                    i_++;
-                }
-                break;
-            case '|':
-                i_++;
-                res = abs(add_(x));
-                if (func_[i_] == '|') {
-                    i_++;
-                }
-                break;
-            default:
-                break;
-        }
-        return res;
-    }
 };
 
 #endif
